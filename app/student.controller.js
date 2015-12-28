da2cca2265de5220a6c2d2156c64c7a2b47c956d
Moath Omar <Moath@foothillsolutions.com>
2015-12-29 00:07:47 +0200

    changing the handler from alert into something nice

--- app/student.controller.js
+++ app/student.controller.js
@@ -34,7 +34,10 @@ function StudentsController($scope){
     this.ctrlStudents = students;
     
     $scope.clickHandler = function clickHandler(student){
-        alert(student.age);        
+       
+        $scope.selectedStudent =  angular.copy(student);
+        
+             
     }
     
 }
